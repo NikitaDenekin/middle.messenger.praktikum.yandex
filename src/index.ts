101824c335ed5a8fc7f009cf0fc4ecd4e7657a30
imDok <denekin.nikita@bk.ru>
2022-03-23 20:33:14 +0700

    Fixed

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
-import "./style.scss";
+import "./style.scss"
 
-import registerComponent from "./utils/registerComponent";
-import Router from "./utils/Router";
-import HomePage from "./pages/home";
-import LoginPage from "./pages/login";
-import SignupPage from "./pages/signup";
-import Block from "./utils/Block";
-import ProfilePage from "./pages/profile";
-import AuthController from "./controllers/AuthController";
+import registerComponent from "./utils/registerComponent"
+import Router from "./utils/Router"
+import HomePage from "./pages/home"
+import LoginPage from "./pages/login"
+import SignupPage from "./pages/signup"
+import Block from "./utils/Block"
+import ProfilePage from "./pages/profile"
+import AuthController from "./controllers/AuthController"
 
 const components = require("./components/**/index.ts") as {
-  [key: string]: { default: typeof Block };
-};
+  [key: string]: { default: typeof Block }
+}
 
 Object.values(components).forEach((component) => {
-  registerComponent(component.default);
-});
+  registerComponent(component.default)
+})
 
 AuthController.fetchUser().then(() => {
-  const router = new Router();
+  const router = new Router()
 
   router
     .use("/", HomePage)
     .use("/login", LoginPage)
     .use("/signup", SignupPage)
     .use("/profile", ProfilePage)
-    .start();
-});
+    .start()
+})
